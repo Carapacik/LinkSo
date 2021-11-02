0254729c2bc1c21293bf0fc41a5644342bbf7a9a
Carapacik <carapacik@gmail.com>
2021-11-02 15:18:49 +0300

    ListView fix

--- ClientFlutter/lib/pages/overview/overview_page.dart
+++ ClientFlutter/lib/pages/overview/overview_page.dart
@@ -1,33 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:linkso/controller_instances.dart';
 import 'package:linkso/helpers/responsiveness.dart';
 import 'package:linkso/pages/overview/widgets/overview_cards_large.dart';
 import 'package:linkso/pages/overview/widgets/overview_cards_medium.dart';
 import 'package:linkso/pages/overview/widgets/overview_cards_small.dart';
 import 'package:linkso/pages/overview/widgets/revenue_section_large.dart';
 import 'package:linkso/pages/overview/widgets/revenue_section_small.dart';
-import 'package:linkso/widgets/custom_text.dart';
 
 class OverviewPage extends StatelessWidget {
   const OverviewPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return ListView(
       children: [
-        ListView(
-          children: [
-            if (ResponsiveWidget.isLargeScreen(context) || ResponsiveWidget.isMediumScreen(context))
-              if (ResponsiveWidget.isCustomSize(context))
-                const OverviewCardsMediumScreen()
-              else
-                const OverviewCardsLargeScreen()
-            else
-              const OverviewCardsSmallScreen(),
-            if (!ResponsiveWidget.isSmallScreen(context)) const RevenueLarge() else const RevenueSmall(),
-          ],
-        ),
+        if (ResponsiveWidget.isLargeScreen(context) || ResponsiveWidget.isMediumScreen(context))
+          if (ResponsiveWidget.isCustomSize(context)) const OverviewCardsMediumScreen() else const OverviewCardsLargeScreen()
+        else
+          const OverviewCardsSmallScreen(),
+        if (!ResponsiveWidget.isSmallScreen(context)) const RevenueLarge() else const RevenueSmall(),
       ],
     );
   }
