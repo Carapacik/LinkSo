595c6e0bb0099ad510d3f5de2f05ccd741b200b5
ImperialMan <1>
2021-11-13 19:45:36 +0300

    fix

--- ServerDotnet/WebApi/ExceptionHandling/ValidationErrorDetails.cs
+++ ServerDotnet/WebApi/ExceptionHandling/ValidationErrorDetails.cs
@@ -4,9 +4,10 @@ namespace WebApi.ExceptionHandling
 {
     public class ValidationErrorDetails: ErrorDetails
     {
+        private const string InvalidFieldsMessage = "Validation.InputFieldsAreInvalid";
         [JsonPropertyName("invalidFields")] public string[] InvalidFields { get; }
         
-        public ValidationErrorDetails(string[] invalidFields): base(400, null)
+        public ValidationErrorDetails(string[] invalidFields): base(400, InvalidFieldsMessage)
         {
             InvalidFields = invalidFields;
         }
